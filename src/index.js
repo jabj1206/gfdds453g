719fe037d7d96a7c60926793b4d6524e424121bc
Jorge Betancur <jabetancur12@gmail.com>
2019-08-27 22:18:23 -0500

    ok

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Router, Route, hashHistory, IndexRedirect} from 'react-router'
-import {App, Page1, Page2, NotFound} from './App';
-import './index.css';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { Router, Route, hashHistory, IndexRedirect } from "react-router";
+import { App, Page1, Page2, NotFound } from "./App";
+import "./index.css";
 
 // Implementa las rutas aca
 
 ReactDOM.render(
-  <App />, // Es posible que tengas que hacer un cambio en esta linea.
-  document.getElementById('root')
+  <App>
+    <Router history={hashHistory}>
+      <Route path="/" component={Page1} />
+      <Route path="Page1" component={Page1} />
+      <Route path="Page2" component={Page2} />
+      <Route path="*" component={NotFound} />
+    </Router>
+  </App>, // Es posible que tengas que hacer un cambio en esta linea.
+  document.getElementById("root")
 );
